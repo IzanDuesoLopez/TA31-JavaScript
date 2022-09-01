6222ead503d44e0f08a7acd746f6b9d58f8d55f4
IzanDuesoLopez <izandueso@gmail.com>
2022-09-01 11:02:03 +0200

    Ejercicio13

---
+++ Ejercicio13/js/codigo.js
@@ -0,0 +1,7 @@
+function alertaDocumento(){ // Funcion que muestra alerta por pantalla
+    alert("Parrafo del documento clicado");
+}
+
+function alertaTablaDos(){ // Funcion que muestra alerta por pantalla
+    alert("Parrafo de la tabla 2 clicado");
+}
